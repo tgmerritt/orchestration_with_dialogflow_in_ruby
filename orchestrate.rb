56e7d8bd575a6e130490aae6f35e9d69dda4f523
Tyler Merritt <tgmerritt@gmail.com>
2020-03-06 16:39:25 +0900

    Fix orchestrate file
    - Why did I symbolize names in the first place?

--- orchestrate.rb
+++ orchestrate.rb
@@ -8,15 +8,12 @@ require_relative 'google_dialog'
 
 before do
   request.body.rewind
-  @request_payload = JSON.parse(request.body.read, symbolize_names: true)
+  @request_payload = JSON.parse(request.body.read, symbolize_names: false)
   @request_payload.each { |name, value| instance_variable_set("@#{name.to_s.tr('-', '_')}", value) }
 end
 
 post '/orchestrate' do
-  
-  puts "Payload is #{@request_payload}"
-  puts "Instance variables are #{instance_variables}"
-  payload = GoogleDialog.new(@fm_conversation, @fm_question, @fm_avatar[:avatarSessionId]).query_dialogflow
+  payload = GoogleDialog.new(@fm_conversation, @fm_question, @fm_avatar["avatarSessionId"]).query_dialogflow
   content_type :json
   payload.to_json
 end
